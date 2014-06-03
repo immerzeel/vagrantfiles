22401bc55e78e2f5096c4ef8597a77782e3474f9
Pascal Immerzeel <pascal@immerzeel.net>
2014-06-03 10:20:09 -0700

    Move Hiera directory
    Also added virtual and OS selection option.

--- puppet/manifests/development.pp
+++ puppet/manifests/development.pp
@@ -1,34 +1,25 @@
-# create a new run stage to ensure certain modules are included first.
-# stage { 'setup':
-#   before => Stage['main']
-# }
-
-# class { 'base':
-#   stage => 'setup'
-# }
-
+hiera_include('classes')
 # Set the global executables path for all the modules.
 Exec { path => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin', '/usr/local/sbin']}
+>
+class utils {
+  $required_apt_packages = [
+    'silversearcher-ag',
+    'pandoc',
+    'imagemagick',
+    'links'
+  ]
 
-$required_apt_packages = [
-  'silversearcher-ag',
-  'pandoc',
-  'imagemagick',
-  'links'
-]
-
-package { $required_apt_packages:
-    ensure => installed,
+  package { $required_apt_packages:
+    ensure => latest,
     provider => apt
+  }
 }
 
-# tmux and tmuxinator
-package{'tmux':}
-package{'tmuxinator':
-  provider => gem
+class tmux {
+  # tmux and tmuxinator
+  package{'tmux':}
+  package{'tmuxinator':
+    provider => gem
+  }
 }
-
-include dotfiles
-include ohmyzsh
-include vim
-
